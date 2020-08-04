6eeeeabad825dfb8708051846fed6be8d16780ae
Srini Godari <49933652+godari3213@users.noreply.github.com>
2020-08-04 08:52:32 +0530

    Update NewTest99.java

--- src/test/java/Demo456/Demo456/NewTest99.java
+++ src/test/java/Demo456/Demo456/NewTest99.java
@@ -5,6 +5,6 @@ import org.testng.annotations.Test;
 public class NewTest99 {
   @Test
   public void f() {
-	  System.out.println("Welcome to DevOps");
+	  System.out.println("Welcome  to DevOps");
   }
 }
